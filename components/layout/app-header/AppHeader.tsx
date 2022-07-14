22fe59eb1c410679864701fff28cec9a52d54b55
Marco Rodriguez <48032991+Rodrig79@users.noreply.github.com>
2022-07-13 17:18:19 -0700

    added logo svg to AppHeader

--- components/layout/app-header/AppHeader.tsx
+++ components/layout/app-header/AppHeader.tsx
@@ -1,17 +1,20 @@
-import styles from 'styles/layout/app-header/AppHeader.module.scss'
+import Image from "next/image";
+import styles from "styles/layout/app-header/AppHeader.module.scss";
+import logoSvg from "public/stackline_logo.svg";
 
-interface Props{
-
-}
+interface Props {}
 
 const AppHeader: React.FC<Props> = ({}) => {
-
-  return(
-    <div className={styles.header} >
-    
-
+  return (
+    <div className={styles.header}>
+      <div className={styles.logo}>
+        <Image 
+        src={logoSvg} 
+        alt="Stackline logo" 
+        />
+      </div>
     </div>
-  )
+  );
 };
 
 export default AppHeader;
